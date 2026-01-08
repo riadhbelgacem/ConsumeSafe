10868967b63670e1d73c4fd8a4434885e277522a
riadhbelgacem <152058296+riadhbelgacem@users.noreply.github.com>
2026-01-08 10:35:57 +0100

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { ConsumeSafeComponent } from './consume-safe/consume-safe.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
